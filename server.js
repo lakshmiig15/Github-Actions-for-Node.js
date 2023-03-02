4fccf1df68f222410b009f9d2cc05845f7d5a606
lakshmiig <lakshmi.ig@thinkbridge.in>
2023-03-02 20:28:52 +0530

    files added

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const ENV = 'DEV';
 const app = express();
 app.get('/', (req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello from GitHub Learning';
+  const msg = 'Hello from GitHub Learning!';
   res.send(getPage(msg));
 });
 
